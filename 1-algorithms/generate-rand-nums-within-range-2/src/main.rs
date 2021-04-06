91b7ed6e1307d2c8fb94f6d894b3b5d476bb5494
frankhart <siddharthadhar.soumen@srmuniv.edu.in>
2021-04-07 00:26:35 +0530

    Add - generate random numbers within a range

---
+++ 1-algorithms/generate-rand-nums-within-range-2/src/main.rs
@@ -0,0 +1,20 @@
+use rand::Rng;
+use rand::distributions::{Distribution, Uniform};
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    println!("Integer: {}", rng.gen_range(0..10));
+    println!("Float: {}", rng.gen_range(0.0..10.0));
+
+    // Generating from uniform distribution has same effect
+    // but maybe faster when repeatedly generating numbers in the same range
+    let die = Uniform::from(1..7);
+
+    loop {
+        let throw = die.sample(&mut rng);
+        println!("Roll the die: {}", throw);
+        if throw == 6 {
+            break;
+        }
+    }
+}
